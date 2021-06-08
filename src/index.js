7c058bba179b0b986ee5333f68cb1c7b787f6db4
Олег Синицин <olejiksin@mail.ru>
2021-06-08 21:23:04 +0300

    d

--- src/index.js
+++ src/index.js
@@ -1,30 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import './css/index.css';
-import './css/log-reg.scss';
-import './css/main.scss';
-import Login from './containers/Login';
-import SignUp from './components/SignUp';
-import * as serviceWorker from './serviceWorker';
-import {storeGlob} from './storeConf';
-import {Provider} from 'react-redux';
-import Profile from './containers/Profile'
-
-const App = () => (
-    <Switch>
-        <Route path='/login' component={Login}/>
-        <Route path='/signUp' component={SignUp}/>
-        <Route path='/applications' component={Profile}/>
-    </Switch>
-);
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <Provider store={storeGlob}>
-        <Router>
-            <App/>
-        </Router>
-    </Provider>
-    , document.getElementById('root'));
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
